b6724f08debb063e8ce775ff233fa564684de550
Hamirall <32832060+Hamirall@users.noreply.github.com>
2018-03-07 22:09:55 +0100

    Ajout du type d'avion et du poid
    Calcul en fonction du type et du poids de l'avion.

--- Script/script.js
+++ Script/script.js
@@ -1,29 +1,96 @@
-function verifField() {
+function typeAvion() {
+	
+	var recup_avion1 = document.getElementById('CRK').checked;
+	var recup_avion2 = document.getElementById('CR7').checked;
+
+	if (recup_avion1 == true) {
+		
+		CRK();
+	} else if (recup_avion2 == true) {
+		
+		CR7();
+	} else {
+		alert("Choisissez un avion");
+	}
+	
+
+}
+
+function CRK() {
+	var recup_poid1 = document.getElementById('superieur1').checked;
+	var recup_poid2 = document.getElementById('sup-egal1').checked;
+
+
+	if (recup_poid1 == true) {
+		
+		var soute1 = -0.5;
+		var soute2 = -0.2;
+		var soute6 = 0.9;
+
+		verifField(soute1,soute2,soute6);
+	
+	} else if (recup_poid2 == true) {
+		
+		var soute1 = -0.7;
+		var soute2 = -0.2;
+		var soute6 = 1.2;
+
+		verifField(soute1,soute2,soute6);
+
+	} else {
+		alert("Choisissez le poid de l'avion");
+	}
+}
+
+function CR7() {
+	var recup_poid1 = document.getElementById('superieur2').checked;
+	var recup_poid2 = document.getElementById('sup-egal2').checked;
+
+	if (recup_poid1 == true) {
+		
+		var soute1 = -0.3;
+		var soute2 = 0;
+		var soute6 = 0.9;
+
+		verifField(soute1,soute2,soute6);
+	
+	} else if (recup_poid2 == true) {
+		
+		var soute1 = -0.4;
+		var soute2 = 0;
+		var soute6 = 1.2;
+
+		verifField(soute1,soute2,soute6);
+
+	} else {
+		alert("Choisissez le poid de l'avion");
+	}
+
+} 
+
+function verifField(soute1,soute2,soute6) {
 
 	var recup_soute1 = document.getElementById('soute1').value;
 	var recup_soute2 = document.getElementById('soute2').value;
 	var recup_soute6 = document.getElementById('soute6').value;
 
-	if (recup_soute1 != "" && recup_soute2 != "" && recup_soute6 != "" ) {
+	if (recup_soute1 == "" && recup_soute2 == "" && recup_soute6 == "" ) {
 
-
-		recupNbr();
+		var recup_soute1 = 0;
+		var recup_soute2 = 0;
+		var recup_soute6 = 0;
+		
+		recupNbr(recup_soute1,recup_soute2,recup_soute6,soute1,soute2,soute6);
 	
 	} else {
-		alert("Veuillez remplir tous les champs");
+		
+		recupNbr(recup_soute1,recup_soute2,recup_soute6,soute1,soute2,soute6);
 	}
 
 }
 
-function recupNbr() {
-	
-	var soute1 = -0.5;
-	var soute2 = -0.2;
-	var soute6 = 0.9;
+function recupNbr(recup_soute1,recup_soute2,recup_soute6,soute1,soute2,soute6) {
 
-	var recup_soute1 = document.getElementById('soute1').value;
-	var recup_soute2 = document.getElementById('soute2').value;
-	var recup_soute6 = document.getElementById('soute6').value;
 
 	var recup_soute1 = recup_soute1/10;
 	var recup_soute2 = recup_soute2/10;
@@ -42,6 +109,7 @@ function recupNbr() {
 		affichage(resultat, soute1, soute2, soute6);
 }
 
+
 function affichage (resultat, soute1, soute2, soute6) {
 
 	
@@ -56,9 +124,6 @@ function affichage (resultat, soute1, soute2, soute6) {
 
 	var affiche_resul = '<p>resultat</p>';
 	document.getElementById('maDiv').innerHTML=resultat;
-
-	
-
 
 }
 
